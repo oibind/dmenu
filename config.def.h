48f3637dccd4bf4b92712c0ace8da3160f48a563
Aoibhinn Nic Aoidh <oibind@pm.me>
2020-08-26 03:16:32 +0100

    Configure colours

--- config.def.h
+++ config.def.h
@@ -13,7 +13,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#3d3d3d", "#dcdccc" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
